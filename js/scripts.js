79ef8772ea4791d1f5cbcd205667cbb920e5daba
Dominic Brown <yeahdom@gmail.com>
2017-03-25 15:17:17 -0700

    add buttons, alternate tiles, add basic jquery button stuff

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+
+
+  // BUTTONS BUTTONS BUTTONS
+  // BUTTONS BUTTONS BUTTONS
+  $("#allon").click(function() {
+    alert("allon");
+  });
+  $("#alloff").click(function() {
+    alert("alloff");
+  });
+  $("#something").click(function() {
+    alert("something");
+  });
+  $("#showboard").click(function() {
+    alert("showboard");
+  });
+  $("#resetpage").click(function() {
+    location.reload();
+  });
+  $("#addrow").click(function() {
+    // alert("addrow");
+
+
+  });
+
+
+
+});
