d2b232ab9a0fe7b28db5cc1759aeef1de5625725
Dominic Brown <yeahdom@gmail.com>
2017-03-25 16:10:33 -0700

    add buttons for backround themes test, fix some buttons, reorganize rows

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,38 @@ $(document).ready(function() {
   // BUTTONS BUTTONS BUTTONS
   // BUTTONS BUTTONS BUTTONS
   $("#allon").click(function() {
-    alert("allon");
+    return 0;
   });
   $("#alloff").click(function() {
-    alert("alloff");
+    return 0;
   });
-  $("#something").click(function() {
-    alert("something");
+  $("#fade").click(function() {
+    $(".row").fadeToggle();
   });
-  $("#showboard").click(function() {
-    alert("showboard");
+  $("#showhide").click(function() {
+    $(".row").toggle();
+    // alert("show hide");
   });
   $("#resetpage").click(function() {
     location.reload();
   });
   $("#addrow").click(function() {
-    // alert("addrow");
-
-
+    return 0;
+  });
+  $("button#greenback").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#yellowback").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+  $("button#redback").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  $("button#resetback").click(function() {
+    $("body").removeClass();
   });
 
 
